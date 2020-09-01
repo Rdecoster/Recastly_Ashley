8c85fe230fb201c81741b6fb16646c670656173f
Ryan De Coster <ryandecoster@gmail.com>
2020-08-31 17:39:15 -0700

    Save point before dinner passing props to VideoListEntry dynamicly rendered

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,15 +1,23 @@
 import VideoListEntry from "./VideoListEntry.js"
 
-var VideoList = () => (
+var VideoList = (props) => {
+  var data = props.data;
+  console.log(data, 'the data in video list');
+
+  const videoEntry = data.map((videoInfo) =>
+
+     <VideoListEntry video = {videoInfo}/>
+
+  )
+
+
+  return (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
-    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
-    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
-    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
-    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
+   {videoEntry}
+
   </div>
 );
-
+}
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
@@ -20,3 +28,10 @@ VideoList.propTypes = {
 // `var` declarations will only exist globally where explicitly defined.
 
 export default VideoList;
+
+
+ /* <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div>
+    <div><h5><em>videoListEntry</em> <VideoListEntry /></h5></div> */
